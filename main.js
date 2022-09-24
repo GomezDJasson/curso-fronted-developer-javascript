0d0ae8043591a6b5098fa813c4cfaf52c593709e
GomezDJasson <fallenangeljd@gmail.com>
2022-09-23 19:40:58 -0500

    Agregamos una lista de productos desde un array

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const aside = document.querySelector('.product-detail');
+const cardsContainer = document.querySelector('.cards-container')
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
@@ -19,7 +20,7 @@ function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
   }
   
-  function toggleMobileMenu() {
+function toggleMobileMenu() {
     const isAsideClosed = aside.classList.contains('inactive');
   
     if (!isAsideClosed) {
@@ -29,7 +30,7 @@ function toggleDesktopMenu() {
     mobileMenu.classList.toggle('inactive');
   }
   
-  function toggleCarritoAside() {
+function toggleCarritoAside() {
     const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
     
     if (!isMobileMenuClosed) {
@@ -38,4 +39,106 @@ function toggleDesktopMenu() {
     
     aside.classList.toggle('inactive');
   }
+
+const productList = [];
+productList.push({
+    name: 'Funko Pop',
+    price: 120,
+    image: 'https://images.pexels.com/photos/4305275/pexels-photo-4305275.jpeg?auto=compress&cs=tinysrgb&h=650&w=940',
+});
+productList.push({
+    name: 'Funko Pop',
+    price: 70,
+    image: 'https://images.pexels.com/photos/4305275/pexels-photo-4305275.jpeg?auto=compress&cs=tinysrgb&h=650&w=940',
+});
+productList.push({
+    name: 'Funko Pop',
+    price: 90,
+    image: 'https://images.pexels.com/photos/4305275/pexels-photo-4305275.jpeg?auto=compress&cs=tinysrgb&h=650&w=940',
+});
+productList.push({
+    name: 'Funko Pop',
+    price: 120,
+    image: 'https://images.pexels.com/photos/4305275/pexels-photo-4305275.jpeg?auto=compress&cs=tinysrgb&h=650&w=940',
+});
+productList.push({
+    name: 'Funko Pop',
+    price: 70,
+    image: 'https://images.pexels.com/photos/4305275/pexels-photo-4305275.jpeg?auto=compress&cs=tinysrgb&h=650&w=940',
+});
+productList.push({
+    name: 'Funko Pop',
+    price: 90,
+    image: 'https://images.pexels.com/photos/4305275/pexels-photo-4305275.jpeg?auto=compress&cs=tinysrgb&h=650&w=940',
+});
+productList.push({
+    name: 'Funko Pop',
+    price: 120,
+    image: 'https://images.pexels.com/photos/4305275/pexels-photo-4305275.jpeg?auto=compress&cs=tinysrgb&h=650&w=940',
+});
+productList.push({
+    name: 'Funko Pop',
+    price: 70,
+    image: 'https://images.pexels.com/photos/4305275/pexels-photo-4305275.jpeg?auto=compress&cs=tinysrgb&h=650&w=940',
+});
+productList.push({
+    name: 'Funko Pop',
+    price: 90,
+    image: 'https://images.pexels.com/photos/4305275/pexels-photo-4305275.jpeg?auto=compress&cs=tinysrgb&h=650&w=940',
+});
+productList.push({
+    name: 'Funko Pop',
+    price: 120,
+    image: 'https://images.pexels.com/photos/4305275/pexels-photo-4305275.jpeg?auto=compress&cs=tinysrgb&h=650&w=940',
+});
+productList.push({
+    name: 'Funko Pop',
+    price: 70,
+    image: 'https://images.pexels.com/photos/4305275/pexels-photo-4305275.jpeg?auto=compress&cs=tinysrgb&h=650&w=940',
+});
+productList.push({
+    name: 'Funko Pop',
+    price: 90,
+    image: 'https://images.pexels.com/photos/4305275/pexels-photo-4305275.jpeg?auto=compress&cs=tinysrgb&h=650&w=940',
+});
+
+function renderProducts(arr) {
+    for (product of arr) {
+      const productCard = document.createElement('div');
+      productCard.classList.add('product-card');
+    
+      // product= {name, price, image} -> product.image
+      const productImg = document.createElement('img');
+      productImg.setAttribute('src', product.image);
+    
+      const productInfo = document.createElement('div');
+      productInfo.classList.add('product-info');
+    
+      const productInfoDiv = document.createElement('div');
+    
+      const productPrice = document.createElement('p');
+      productPrice.innerText = '$' + product.price;
+      const productName = document.createElement('p');
+      productName.innerText = product.name;
+    
+      productInfoDiv.appendChild(productPrice);
+      productInfoDiv.appendChild(productName);
+    
+      const productInfoFigure = document.createElement('figure');
+      const productImgCart = document.createElement('img');
+      productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+    
+      productInfoFigure.appendChild(productImgCart);
+    
+      productInfo.appendChild(productInfoDiv);
+      productInfo.appendChild(productInfoFigure);
+    
+      productCard.appendChild(productImg);
+      productCard.appendChild(productInfo);
+    
+      cardsContainer.appendChild(productCard);
+    }
+  }
+
+  renderProducts(productList);
 
